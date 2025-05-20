488e89484f009501a531956ce446fd345171f337
Ylana Oliveira <ylanalmo@gmail.com>
2025-05-20 11:20:04 -0300

    values and variables

---
+++ values-and-variables/index.js
@@ -0,0 +1,12 @@
+// Values and Variables
+
+//   1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+//   2. Log their values to the console
+
+const country = 'Brazil';
+const continent = 'South America';
+const population = 211000000;
+
+console.log(
+  `I live in ${country}, located in ${continent}, with a population of ${population} people.`,
+);
